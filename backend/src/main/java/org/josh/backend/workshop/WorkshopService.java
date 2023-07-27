99ddfbd6d9b1eaa93a8bf6450cbf8d89f27c659c
Josh <joshuastolle445@gmail.com>
2023-07-27 19:52:30 +0200

    alter put method logic

--- backend/src/main/java/org/josh/backend/workshop/WorkshopService.java
+++ backend/src/main/java/org/josh/backend/workshop/WorkshopService.java
@@ -34,20 +34,42 @@ public class WorkshopService {
         return workshopRepository.findAll();
     }
 
-    public Workshop updateWorkshop(String id, WorkshopWithoutIdAndLikes workshop) {
+    public Workshop updateWorkshopPersonalStatus(String id, WorkshopPersonalStatus workshopPersonalStatus){
         Workshop workshopBefore = workshopRepository.findById(id).orElseThrow();
+        List<WorkshopPersonalStatus> workshopPersonalStatuses = getWorkshopPersonalStatuses(workshopPersonalStatus, workshopBefore);
+
         Workshop workshopToSave = new Workshop(
-                id,
-                new MongoUserWithoutPassword("adminId", "AdminName"),
-                workshop.topic(),
-                workshop.subTopic(),
-                workshop.buzzWords(),
-                workshopBefore.likes(),
-                workshop.estimatedTimeToMaster(),
-                workshop.difficulty(),
-                workshopBefore.workshopPersonalStatuses()
+            workshopBefore.id(),
+            workshopBefore.author(),
+            workshopBefore.topic(),
+            workshopBefore.subTopic(),
+            workshopBefore.buzzWords(),
+            workshopBefore.likes(),
+            workshopBefore.estimatedTimeToMaster(),
+            workshopBefore.difficulty(),
+            workshopPersonalStatuses
         );
         return workshopRepository.save(workshopToSave);
+    }
+
+    private static List<WorkshopPersonalStatus> getWorkshopPersonalStatuses(WorkshopPersonalStatus workshopPersonalStatus, Workshop workshopBefore) {
+        List<WorkshopPersonalStatus> workshopPersonalStatuses = new ArrayList<>(workshopBefore.workshopPersonalStatuses());
+
+        int indexToUpdate = -1;
+        for (int i = 0; i < workshopPersonalStatuses.size(); i++) {
+            WorkshopPersonalStatus personalStatus = workshopPersonalStatuses.get(i);
+            if (personalStatus.user().equals(workshopPersonalStatus.user())) {
+                indexToUpdate = i;
+                break;
+            }
+        }
+
+        if (indexToUpdate == -1) {
+            workshopPersonalStatuses.add(workshopPersonalStatus);
+        } else {
+            workshopPersonalStatuses.set(indexToUpdate, workshopPersonalStatus);
+        }
+        return workshopPersonalStatuses;
     }
 
 }
