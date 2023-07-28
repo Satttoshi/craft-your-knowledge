8b3996d53c64009ef394f46f4eaefbf2d0c78d21
Josh <joshuastolle445@gmail.com>
2023-07-28 23:14:56 +0200

    add github flavoured markdown

--- frontend/src/components/ContentField.tsx
+++ frontend/src/components/ContentField.tsx
@@ -1,9 +1,41 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 type Props = {
-    content: string
-}
+    content: string;
+};
 
 export default function ContentField({content}: Props) {
 
-    return <p>{content}</p>;
+    console.log(content)
 
+    const testMd = `# instagram-on-steroids
+
+This is an automation for instagram to daily like
+content based on different hashtags
+
+## Setup Guide
+
+Requirements:
+
+- need Chrome installed
+- need Node.js installed
+
+* [x] todo
+
+1. Create .env file.
+2. Insert into it:
+
+\`\`\`json
+IG_USERNAME="insertUsername"
+IG_PASSWORD="insertPassword"
+\`\`\`
+
+3. Open terminal within the directory of index.js
+4. Run with \`node index\`
+`;
+
+    return (
+        <ReactMarkdown children={testMd} remarkPlugins={[remarkGfm]}/>
+    );
 }
