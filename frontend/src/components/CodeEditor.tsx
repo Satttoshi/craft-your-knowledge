325d99bc0f2de5eba41c7cbcd912fdf897516da5
Josh <joshuastolle445@gmail.com>
2023-08-02 21:51:00 +0200

    add some settings to code editor

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,12 +1,23 @@
 import Editor from '@monaco-editor/react';
+import {useState} from "react";
 
 type Props = {
     language: string;
 }
 
 export default function CodeEditor({language}: Props) {
+    const [code, setCode] = useState<string | undefined>("// start coding here ...");
 
     const editorLanguage = language.toLowerCase();
 
-    return <Editor height="90vh" defaultLanguage={editorLanguage} defaultValue="" />
+    return <form><Editor
+        height="80vh"
+        width="60vw"
+        theme="hc-black"
+        defaultLanguage={editorLanguage}
+        defaultValue="// start coding here ..."
+        onChange={(value) => setCode(value)}
+    />
+    </form>
+
 }
