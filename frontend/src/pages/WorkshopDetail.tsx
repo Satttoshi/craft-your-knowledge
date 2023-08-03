05f03e14f2bba9b6e4ec6f6a77dbf7dde4b7d937
Josh <joshuastolle445@gmail.com>
2023-08-04 01:46:40 +0200

    add back button

--- frontend/src/pages/WorkshopDetail.tsx
+++ frontend/src/pages/WorkshopDetail.tsx
@@ -1,10 +1,12 @@
 import LikeStar from "../components/LikeStar.tsx";
 import DeleteButton from "../components/DeleteButton.tsx";
 import ContentField from "../components/ContentField.tsx";
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import {useStore} from "../hooks/useStore.ts";
 import NotFound from "../pages/NotFound.tsx";
 import CodeEditor from "../components/CodeEditor.tsx";
+import styled from "@emotion/styled";
+import Button from '@mui/material/Button';
 
 export default function WorkshopDetail() {
 
@@ -12,6 +14,7 @@ export default function WorkshopDetail() {
     const getWorkshopById = useStore((state) => state.getWorkshopById);
     const readWorkshopById = useStore((state) => state.readWorkshopById);
     const fetchedWorkshop = useStore((state) => state.workshop);
+    const navigate = useNavigate();
 
     let currentWorkshop;
     try {
@@ -31,13 +34,39 @@ export default function WorkshopDetail() {
 
     const articleAndChallenge = currentWorkshop.article.choices[0].message.content + "\n" + currentWorkshop.challenge.choices[0].message.content;
 
+    function handleBack() {
+        navigate(-1);
+    }
+
     return (<>
-        <h2>Workshop</h2>
-        <p>Language: {currentWorkshop.language}</p>
-        <p>Topic: {currentWorkshop.topic}</p>
+        <StyledBanner>
+            <Button onClick={handleBack} variant="outlined"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--color4)"><path d="M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z" /></svg></Button>
+            <h2>Workshop</h2>
+            <p>Language: {currentWorkshop.language}</p>
+            <p>Topic: {currentWorkshop.topic}</p>
+        </StyledBanner>
         <LikeStar workshop={currentWorkshop}/>
         <DeleteButton id={currentWorkshop.id}/>
         <ContentField content={articleAndChallenge}/>
         <CodeEditor workshop={currentWorkshop}/>
     </>)
 }
+
+const StyledBanner = styled.div`
+  width: 95vw;
+  background-color: var(--color3);
+  border-radius: 10px;
+  display: flex;
+
+  h2 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+
+
+
+
+`;
