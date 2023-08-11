84df934be53d942a528f9fe718c0c520ed3d00e1
Josh <joshuastolle445@gmail.com>
2023-08-12 01:42:33 +0200

    remove unused imports

--- backend/src/main/java/org/josh/backend/security/MongoUserController.java
+++ backend/src/main/java/org/josh/backend/security/MongoUserController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.josh.backend.dto.UserWithoutId;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
