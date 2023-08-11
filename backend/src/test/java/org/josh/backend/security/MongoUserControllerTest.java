f17b0113e036114e434fa397e33a9ed381a8fa48
Josh <joshuastolle445@gmail.com>
2023-08-12 00:53:55 +0200

    complete MongoUserControllerTest

--- backend/src/test/java/org/josh/backend/security/MongoUserControllerTest.java
+++ backend/src/test/java/org/josh/backend/security/MongoUserControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -84,7 +83,7 @@ class MongoUserControllerTest {
         //GIVEN
         String testUserWithoutId = """
                 {
-                    "name": "testUser",
+                    "username": "testUser",
                     "password": "secretPass3"
                 }
             """;
