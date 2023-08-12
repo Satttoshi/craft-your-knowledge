87df1575a4de451e2c32333f5b1b60c8bd10b603
Josh <joshuastolle445@gmail.com>
2023-08-13 01:14:33 +0200

    make sure that the jwtFilter runs before FilterChain

--- backend/src/main/java/org/josh/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/josh/backend/security/SecurityConfig.java
@@ -12,13 +12,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(httpSecuritySessionManagementConfigurer ->
