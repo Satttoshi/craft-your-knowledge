eb2a03f6c938c81db9cd1507e1076e9707b2c2e8
Josh <joshuastolle445@gmail.com>
2023-08-14 17:13:55 +0200

    dependency injection for argon encoder and jwtfilter

--- backend/src/main/java/org/josh/backend/security/MongoUserDetailsService.java
+++ backend/src/main/java/org/josh/backend/security/MongoUserDetailsService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +17,8 @@ import java.util.Collections;
 public class MongoUserDetailsService implements UserDetailsService{
 
     private final MongoUserRepository mongoUserRepository;
-
-    private final IdService idService = new IdService();
+    private final IdService idService;
+    private final PasswordEncoder encoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -30,10 +29,8 @@ public class MongoUserDetailsService implements UserDetailsService{
     }
 
     public void registerNewUser(UserWithoutId userWithoutId) {
-        PasswordEncoder encoder = new Argon2PasswordEncoder(16, 32, 8, 1 << 16, 4);
         String encodedPassword = encoder.encode(userWithoutId.password());
-
-        MongoUser newUser = new MongoUser(idService.createId() ,userWithoutId.username(), encodedPassword);
+        MongoUser newUser = new MongoUser(idService.createId(), userWithoutId.username(), encodedPassword);
         mongoUserRepository.save(newUser);
     }
 
